8112d7936dd3a3de698e42583e580e737bfcdf5c
Arshu Mardi <130129383+arrsu@users.noreply.github.com>
2023-04-21 23:09:58 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+//client.on('message', message => {
+//	if(message.body === '!ping') {
+//		message.reply('pong');
+//	}
+//});
+
+
+//client.on('message', message => {
+//    if(message.body === 'Hi') {
+//        message.reply('hlo');
+//    } else {
+//        message.reply('Buddhu');
+//    }
+//});
+
+client.on('message', message => {
+	if(message.body === 'hii') {
+		  message.reply('Yup');
+    } else {
+          message.reply('Bolo na');
+    }
+});
+client.initialize();
